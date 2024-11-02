966d77a6ee948477f89623c36cd3bfa64196b8ee
Daniya <gafurova.daniya@gmail.com>
2024-11-02 17:09:21 +0100

    winLoses + media queries

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let score = 0;
+let winsLoses = [0, 0, 0];
 const choices = ["rock", "scissors", "paper"];
 const winMatrix = 
 [
@@ -15,6 +16,8 @@ function play(buttonId){
     const playerChoice = choices.indexOf(buttonId);
     const whoWon = winMatrix[playerChoice][computerChoice];
     score = score + whoWon;
+    const index = whoWon + 1;
+    winsLoses[index]++;
     console.log(choices[playerChoice], " vs ", choices[computerChoice], " ->", whoWon);
     const opponents = document.querySelectorAll(".opponent");
     for (let opponent of opponents)
@@ -25,11 +28,19 @@ function play(buttonId){
     const results = document.getElementById("results").children;
     for (let result of results)
         result.classList.remove("active");
-    results[whoWon+1].classList.add("active");
+    results[index].classList.add("active");
 
-    const points = document.getElementById("points");
-    points.textContent = score.toString();
+    setValue("points", score);
+    setValue("wins", winsLoses[2]);
+    setValue("draws", winsLoses[1]);
+    setValue("loses", winsLoses[0]);
+    
 }
+function setValue(id, value){
+    const element = document.getElementById(id);
+    element.textContent = value;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll(".button");
     function onClick(event)
